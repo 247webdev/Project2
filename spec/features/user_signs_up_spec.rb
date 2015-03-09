aa7590435cf34934dd5587ef74f6a5c436247563
ReidasaurusRex <reidhlewis91@gmail.com>
2015-03-09 10:59:07 -0700

    Added some more scenarios to user sign up

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -39,5 +39,41 @@ feature 'Visitor creates profile' do
     expect(page).to have_content("Category")
     expect(page).to have_content("Logout")
   end
+
+  scenario 'no password' do
+    # setup
+    visit root_path
+    click_button "Signup with email"
+
+    # exercise
+    fill_in "First Name", with: "Jim"
+    fill_in "Last Name", with: "Bob"
+    fill_in "Email", with: "Jim@email.com"
+    fill_in "Zip Code", with: "86001"
+    fill_in "Password", with: ""
+    click_button "Submit"
+
+    # verify
+    expect(page).to have_content("Please enter a password")
+    expect(page).to have_content("Create a Profile")
+  end
+
+  scenario 'no first name' do
+    # setup
+    visit root_path
+    click_button "Signup with email"
+
+    # exercise
+    fill_in "First Name", with: ""
+    fill_in "Last Name", with: "Bob"
+    fill_in "Email", with: "Jim@email.com"
+    fill_in "Zip Code", with: "86001"
+    fill_in "Password", with: "1234"
+    click_button "Submit"
+
+    #verify
+    expect(page).to have_content("Please enter a first name")
+    expect(page).to have_content("Create a Profile")
+  end
 end
 
