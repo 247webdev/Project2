ec60806c730b2a960009b6e5a658db439b248116
Arebeth Pease <arebethpease@gmail.com>
2015-03-09 15:41:53 -0700

    created search controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   def search #search by category or location
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'searches/results'
+
   resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
