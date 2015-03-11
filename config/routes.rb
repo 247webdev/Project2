0240ef2dda274aa3078cf820f15ad5ea149573b8
Christian Chandler <247webdev+github@googlemail.com>
2015-03-10 23:24:08 -0700

    noticed there were some routes added so, I updated the rake routes comment in the file.

--- config/routes.rb
+++ config/routes.rb
@@ -19,12 +19,18 @@ Rails.application.routes.draw do
 end
 
 # From rake routes
-#    Prefix Verb   URI Pattern               Controller#Action
-#     users GET    /users(.:format)          users#index
-#           POST   /users(.:format)          users#create
-#  new_user GET    /users/new(.:format)      users#new
-# edit_user GET    /users/:id/edit(.:format) users#edit
-#      user GET    /users/:id(.:format)      users#show
-#           PATCH  /users/:id(.:format)      users#update
-#           PUT    /users/:id(.:format)      users#update
-#           DELETE /users/:id(.:format)      users#destroy
+#        Prefix Verb URI Pattern                                  Controller#Action
+#        landingpage GET    /                                     access#landingpage
+# access_landingpage GET    /access/landingpage(.:format)         access#landingpage
+#     searches_index GET    /searches/index(.:format)             searches#index
+#   searches_results POST   /searches/results(.:format)           searches#results
+#                    GET    /searches/results/:category(.:format) searches#resultsShow
+#              users GET    /users(.:format)                      users#index
+#                    POST   /users(.:format)                      users#create
+#           new_user GET    /users/new(.:format)                  users#new
+#          edit_user GET    /users/:id/edit(.:format)             users#edit
+#               user GET    /users/:id(.:format)                  users#show
+#                    PATCH  /users/:id(.:format)                  users#update
+#                    PUT    /users/:id(.:format)                  users#update
+#                    DELETE /users/:id(.:format)                  users#destroy
+#               gift PATCH  /users/:id/edit(.:format)             gifts#update
