c786e2ec1109df288f37efad4ea3a12cd6ccac14
Colin Craft <colin@colincraft.com>
2015-03-12 16:02:48 -0700

    Fixing redirect - sorrrrrry, trying to refactor like Alex is saying :-(.  ¯\_(ツ)_/¯

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
     User.destroy(session[:user_id])
     session[:user_id] = nil
     flash[:notice] = "You deleted your account."
-    redirect_to root
+    redirect_to '/'
   end
 
 
